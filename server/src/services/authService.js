d2006c2216bf5519e8c5e0de38bea8322461bbc7
Amalsha <amalshpro@gmail.com>
2024-12-19 23:35:18 +0530

    auth backend token update

--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -1,9 +1,9 @@
 
 const { User } = require('../models');
-const {updateUserById} = require('../services/userService');
 require('dotenv').config();
 const CustomError = require('../utils/customError');
 const { generateAccessToken, generateRefreshToken, verifyToken } = require('../utils/jwtUtils');
+
 
 const refreshTokensStore = [];
 
@@ -12,24 +12,21 @@ const isUserExist = async (email) => {
   return await User.findOne({ email }).select('-__v');
 };
 
-
 const registerUser = async (name, email, password) => {
 
   // Check if the user already exists
   const existingUser = await isUserExist(email);
   if (existingUser) {
     throw new CustomError("User already exists", 400);
-    
+
   }
-  
-  const user = await User.create({ name, email, password, role:"commuter" });
-  
+
+  const user = await User.create({ name, email, password, role: "commuter" });
 
 
-  return {statusCode:201};
+
+  return { statusCode: 201 };
 };
-
-
 
 const loginUser = async (email, password) => {
   const user = await isUserExist(email);
@@ -38,46 +35,54 @@ const loginUser = async (email, password) => {
     throw new CustomError("Invalid credentials", 400);
   }
 
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user);
+
+  // Store refresh token (replace with DB storage in production)
+  refreshTokensStore.push(refreshToken);
 
 
-  
 
-   const accessToken = generateAccessToken(user);
-    const refreshToken = generateRefreshToken(user);
+  // Convert Mongoose document to plain object
+  const userObj = user.toObject();
 
-    // Store refresh token (replace with DB storage in production)
-    refreshTokensStore.push(refreshToken);
+  // Remove sensitive fields
+  delete userObj.password;
 
-    
-   // Convert Mongoose document to plain object
-   const userObj = user.toObject();
-
-   // Remove sensitive fields
-   delete userObj.password;
-
-    return { user:userObj , accessToken, refreshToken };
+  return { user: userObj, accessToken, refreshToken };
 };
 
-const refreshAccessToken = (refreshToken) => {
+const refreshTokens = (refreshToken) => {
 
   if (!refreshToken) {
     throw new CustomError("Refresh token missing", 401);
-}
+  }
+
+  if(!refreshTokensStore.includes(refreshToken)){
+    throw new CustomError("Refresh token is not valid", 401);
+  }
+
   // Verify refresh token
   const decoded = verifyToken(refreshToken, process.env.JWT_REFRESH_SECRET);
 
   // Generate a new access token
   const newAccessToken = generateAccessToken({ id: decoded.id, role: decoded.role });
 
-  return newAccessToken;
+  const newRefreshToken = generateRefreshToken({ id: decoded.id, role: decoded.role });
+
+
+  revokeRefreshToken(refreshToken);
+  refreshTokensStore.push(newRefreshToken);
+
+  return {newAccessToken, newRefreshToken};
 };
 
 const revokeRefreshToken = (refreshToken) => {
   // Remove refresh token from store
   const index = refreshTokensStore.indexOf(refreshToken);
   if (index !== -1) {
-      refreshTokensStore.splice(index, 1);
+    refreshTokensStore.splice(index, 1);
   }
 };
-  
-  module.exports = { loginUser, registerUser, refreshAccessToken, revokeRefreshToken };
+
+module.exports = { loginUser, registerUser, refreshTokens, revokeRefreshToken };
