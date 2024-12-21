542723f86983a6cc595d04024c603c174210c866
RoshanGunarathna <roshanrnofficial@gmail.com>
2024-12-22 01:18:48 +0530

    route management complete

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,7 +1,9 @@
 const User = require('./userModel');
 const Bus = require('./busModel');
+const Route = require('./routeModel');
 
 module.exports = {
   User,
   Bus,
+  Route
 };
---
+++ server/src/services/routeManagementService.js
@@ -0,0 +1,102 @@
+
+const { Route } = require('../models');
+const CustomError = require('../utils/customError');
+
+
+const isRouteExist = async (routeId) => {
+  return await Route.findOne({ routeId }).select('-__v');
+};
+
+const addARoute = async (routeId, name) => {
+  try {
+
+    console.log(`routeId ${routeId} : name ${name}`)
+    const existingRoute = await isRouteExist(routeId);
+    if (existingRoute) {
+      throw new CustomError("Route already exists", 400);
+  
+    }
+  
+
+  await Route.create({routeId,name});
+    
+    return {statusCode: 201};
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+const getRouteById = async (data) => {
+  try {
+    const route = await Route.findById(data.id).select('-__v');
+    
+    if (!route) {
+      throw new CustomError("Route not found", 404);
+    }
+
+    return route;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+
+
+const updateRouteById = async (data) => {
+  try {
+  
+    const updatedRoute = await Route.findByIdAndUpdate(data.body.id, data.body, {
+      new: true, // Return the updated document
+      runValidators: true, // Run schema validators on the update
+    }).select('-__v');
+
+    if (!updatedRoute) {
+      throw new CustomError("Route not found", 404);
+    }
+   
+    return updatedRoute;
+  } catch (error) {
+  
+    throw error;
+  }
+};
+
+
+const deleteRouteById = async (id) => {
+  try {
+    const existingRoute = await Route.findById(id);
+    if (!existingRoute) {
+      throw new CustomError("Route not found", 404);
+    }
+
+     await Route.findByIdAndDelete(id).select('-__v');
+   
+    return null;
+  } catch (error) {
+   
+    throw error;
+  }
+};
+
+
+const getRoutes = async () => {
+  try {
+    const routeList =  await Route.find({}).select('-__v');
+    return routeList;
+  } catch (error) {
+   
+    throw error;
+  }
+};
+
+
+module.exports = {
+  getRouteById,
+  updateRouteById,
+  deleteRouteById,
+  getRoutes,
+  addARoute
+};
+
